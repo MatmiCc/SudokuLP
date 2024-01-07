129bf9e165761354cc647b085acfeef630aa71ef
MatmiCc <104356093+MatmiCc@users.noreply.github.com>
2024-01-07 19:52:13 +0100

    Update main.py

--- sudoku/main.py
+++ sudoku/main.py
@@ -60,8 +60,6 @@ prob += choices["1"]["5"]["9"] == 1, ""
 prob += choices["6"]["6"]["9"] == 1, ""
 prob += choices["5"]["8"]["9"] == 1, ""
 
-prob.writeLP("Sudoku.lp")  #printing solved sudoku to Sudoku.lp
-
 prob.solve()
 
 # print status(Optimal, Infeasible, Unbounded)
